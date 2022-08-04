c2b5fa2849defc76bc70508eb11239f8aeb0b9e5
fheezy <farhanhoque764@yahoo.com>
2022-08-04 18:44:24 -0500

    defined columns in category and changed few things in connection.js

--- models/Category.js
+++ models/Category.js
@@ -3,10 +3,19 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+//defining the columns
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    }
   },
   {
     sequelize,
