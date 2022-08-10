ec9afb6b3eb148290834f988441cc188f027e435
fheezy <farhanhoque764@yahoo.com>
2022-08-10 01:05:37 -0500

    added a 'findOne' for the tags along with creating a tag 'tag.create'

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,11 +23,33 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+Tag.findOne({
+      where: {
+      id: req.params.id
+    }, //locate single tag by id
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+.then(dbTagData => {
+   if (!dbTagData) {
+    res.status(404).json({ message: 'tag was not found with this id' });
+    }
+  res.status(200).json(dbTagData);
+  })
+  .catch(err => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-});
+ Tag.create({ 
+  tag_name: req.body.tag_name
+ })   
+ .then(dbTagData => res.json(dbTagData));
+}); //vector stores the tag
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
